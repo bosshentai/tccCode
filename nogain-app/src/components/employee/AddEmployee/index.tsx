bf8b080fab873262bc471b30722947f4aef3f2e2
bosshentai <baptistamhernani@gmail.com>
2022-02-08 17:47:42 -0100

    the form to add employee had been started input hover and focus have been done the close button hover have been done the submit button css in not finished

--- nogain-app/src/components/employee/AddEmployee/index.tsx
+++ nogain-app/src/components/employee/AddEmployee/index.tsx
@@ -1,5 +1,70 @@
+import styles from "./styles.module.scss";
 
+import whiteCross from "../../../assets/icons/whiteCross.svg";
+import blueCross from "../../../assets/icons/blueCross.svg";
+import { useState } from "react";
 
 export const AddEmployee = () => {
-  return  <div>ADD PPL</div>
-}
+  const [isHover, setHover] = useState(false);
+
+  const hoverHandler = () => {
+    setHover(true);
+  };
+
+  const leaveHandler = () => {
+    setHover(false);
+  };
+
+  const iconChange = !isHover ? whiteCross : blueCross;
+
+  return (
+    <div className={styles.addEmployeeContainer}>
+      <div className={styles.headerContainer}>
+        <div className={styles.left}>
+          <h1>Inscrição do Funcionario</h1>
+        </div>
+        <button
+          className={styles.right}
+          onMouseEnter={hoverHandler}
+          onMouseLeave={leaveHandler}
+        >
+          {/* <button>Close</button> */}
+          <img src={iconChange} />
+        </button>
+      </div>
+
+      <div className={styles.formContainer}>
+        <div className={styles.inputContainer}>
+          <label>Nome Completo</label>
+          <input type="text" />
+        </div>
+        <div className={styles.twoinputrow}>
+          <div className={styles.inputContainer}>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+          <div className={styles.inputContainer}>
+            <label>Telemóvel</label>
+            <input type="tel" />
+          </div>
+        </div>
+        <div className={styles.twoinputrow}>
+          <div className={styles.inputContainer}>
+            <label>CNI</label>
+            <input type="text" />
+          </div>
+          <div className={styles.inputContainer}>
+            <label>NIF</label>
+            <input type="text" />
+          </div>
+        </div>
+        <div className={styles.inputContainer}>
+          <label>Data de Nascimento</label>
+          <input type="date" />
+        </div>
+
+        <button className={styles.btnSubmit}>Inscrever</button>
+      </div>
+    </div>
+  );
+};
