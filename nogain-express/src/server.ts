b9fdddbdaf5517d4b6f2a6cf8367d8a6372be274
bosshentai <baptistamhernani@gmail.com>
2022-03-07 12:55:34 -0100

    the data base will be use in the working will be postgress docker been done for now

---
+++ nogain-express/src/server.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import express from 'express';
+
+
+
+
+
+
+// const bodyParser = require('body-parser');
+// const HttpError = require('./models/http-error');
+
+// let express = require('express');
+// const employeeRoutes = require('./routes/employee-routes');
+
+
+// const app = express();
+
+// const port = 3000;
+
+// app.use(bodyParser.json());
+
+
+
+
+// app.use((req, res, next) => {
+//   const error = new HttpError('Not found',404);
+//   throw error;
+// })
+
+
+
+
+// app.listen(port);
