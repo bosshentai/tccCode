3d0e0f209760b55e44de3a35453241b53fc0fb49
Bosshentai <baptistamhernani@gmail.com>
2022-07-05 15:01:21 -0100

    start using storybook in Client

---
+++ client/src/stories/Employee/components/EmployeeItem.stories.tsx
@@ -0,0 +1,20 @@
+
+import { EmployeeItem } from "../../../Components/Employee/components/EmployeeItem";
+
+
+export default {
+  title: "EmployeeItem",
+  component: EmployeeItem
+}
+
+
+// const Template = () => <EmployeeItem key="1" id="1" name="nome" 
+// email={"email"}
+// status="Active"
+// />
+
+
+export const Item = () => <EmployeeItem key="1" id="1" name="nome"
+  email={"email"}
+  status="Active"
+/>
