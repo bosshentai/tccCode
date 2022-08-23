e801bc2fcfa3de0516a7d8e633a250be30be91b9
Bosshentai <baptistamhernani@gmail.com>
2022-08-23 16:17:25 -0100

    the createClientController have been started

--- server/src/controllers/Client/controllers/CreateClientController.ts
+++ server/src/controllers/Client/controllers/CreateClientController.ts
@@ -1,41 +1,81 @@
+import { Roles } from '@prisma/client'
+import { Request, Response, NextFunction } from 'express'
+import { validationResult } from 'express-validator'
 
-import {  Roles } from "@prisma/client";
-import { Request, Response, NextFunction } from 'express';
-
-
-import { prismaClient } from "../../../database/prismaClient";
-import { HttpError } from "../../../models/http-error";
-
+import { prismaClient } from '../../../database/prismaClient'
+import { HttpError } from '../../../models/http-error'
 
 export class CreateClientController {
-  async handle(request: Request, response: Response, next: NextFunction) {
+  async handle(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    if (request.method !== 'POST') {
+      const error = new HttpError('Method not allowed', 405)
+      return next(error)
+    }
 
-    if (request.method !== "POST") {
-      const error = new HttpError("Method not allowed", 405);
-      return next(error);
+    const errors = validationResult(request)
+
+    if (!errors.isEmpty()) {
+      return next(
+        new HttpError(
+          'Invalid inputs passed, please check your data.',
+          422,
+        ),
+      )
+    }
+
+    const {
+      name,
+      email,
+      phone,
+      CNI,
+      NIF,
+      birth,
+      trainingplanId,
+      personalTrainerId,
+      discountId,
+    } = request.body
+
+
+
+
+    let existingUser
+
+    try {
+      existingUser = await prismaClient.user.findUnique({
+        where:{
+          email: email
+        }
+      })
+      
+    } catch (e) {
+      const error = new HttpError(
+        "Couldn't register the client.",
+        500
+      )
+      
+      return next(error)
+    }
+
+
+
+    if (existingUser){
+      const error = new HttpError(
+        "User exists already",
+      422
+      )
     }
 
     try {
-
-      const {
-        name,
-        email,
-        phone,
-        CNI,
-        NIF,
-        birth,
-        trainingplanId,
-        personalTrainerId,
-        discountId
-      }
-        = request.body;
-
 
       const user = await prismaClient.user.create({
         data: {
           name: name,
           email: email,
-          password: "123456",
+          password: '123456',
           phone: phone,
           role: Roles.CLIENT,
           CNI: CNI,
@@ -45,40 +85,28 @@ export class CreateClientController {
             create: {
               cpt: {
                 create: {
-                  personal_trainer_id: personalTrainerId
-                }
+                  personal_trainer_id: personalTrainerId,
+                },
               },
               ctp: {
                 create: {
-                  training_plan_id: trainingplanId
-                }
+                  training_plan_id: trainingplanId,
+                },
               },
               cd: {
                 create: {
-                  discount_id: discountId
-                }
-              }
-            }
-          }
+                  discount_id: discountId,
+                },
+              },
+            },
+          },
         },
-   
       })
 
-
-
-    
-      return response.status(201).json(user);
-
+      return response.status(201).json(user)
     } catch (e) {
-
-      const error = new HttpError("Fail to add Client", 500);
-      return next(error);
+      const error = new HttpError('Fail to add Client', 500)
+      return next(error)
     }
   }
 }
-
-
-
-
-
-
