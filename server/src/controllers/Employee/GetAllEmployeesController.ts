d1432add9f48eff3a4ed7f2ccd99c19878803ad4
Bosshentai <baptistamhernani@gmail.com>
2022-09-13 20:17:40 -0100

    The GetAllClientController and GetAllEmployeeController havve been updated

--- server/src/controllers/Employee/GetAllEmployeesController.ts
+++ server/src/controllers/Employee/GetAllEmployeesController.ts
@@ -1,36 +1,45 @@
-import { Employee, prisma, Roles, Status } from "@prisma/client";
+import {
+  Employee,
+  prisma,
+  Roles,
+  Status,
+} from '@prisma/client'
 
-
-import { NextFunction, Request, Response } from "express";
-import { prismaClient } from "../../database/prismaClient";
-import { HttpError } from "../../models/http-error";
-
-
-
-
+import { NextFunction, Request, Response } from 'express'
+import { prismaClient } from '../../database/prismaClient'
+import { HttpError } from '../../models/http-error'
 
 export class GetAllEmployeesController {
-  async handle(request: Request, response: Response, next: NextFunction) {
-
-    if (request.method !== "GET"){
-      const error = new HttpError("Method not allowed", 405)
-      return next(error);
+  async handle(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    if (request.method !== 'GET') {
+      const error = new HttpError('Method not allowed', 405)
+      return next(error)
     }
 
     try {
-
-      const employees = await prismaClient.employee.findMany({
+      const data = await prismaClient.employee.findMany({
         select: {
           id: true,
           status: true,
-          user:{
-            select:{
+          user: {
+            select: {
               name: true,
               email: true,
+            },
+          },
+        },
+      })
 
-            }
-          }
-
+      const employee = data.map((item) => {
+        return {
+          id : item.id,
+          status: item.status,
+          name: item.user.name,
+          email: item.user.email
         }
       })
       // const employees = await prismaClient.user.findMany({
@@ -49,11 +58,11 @@ export class GetAllEmployeesController {
       //   },
       // });
 
-      return response.json(employees);
-
+      return response.json(employee)
     } catch (e) {
-      return response.status(400).json({ error: "Unknown error" });
+      return response
+        .status(400)
+        .json({ error: 'Unknown error' })
     }
-
   }
 }
