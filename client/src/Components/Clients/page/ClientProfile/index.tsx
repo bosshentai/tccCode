45fa71477a73449f1b8afb3cce287dc2c9a98bb6
Bosshentai <baptistamhernani@gmail.com>
2022-12-08 08:55:28 -0100

    fixed the routes for the post the payment

--- client/src/Components/Clients/page/ClientProfile/index.tsx
+++ client/src/Components/Clients/page/ClientProfile/index.tsx
@@ -15,7 +15,9 @@ import { Backdrop } from '../../../shared/components/UIElements/Backdrop'
 import { Payment } from '../Payment'
 // import AxiosResponse from 'axios';
 
-const portalElement = document.getElementById('overlays') as HTMLElement;
+const portalElement = document.getElementById(
+  'overlays',
+) as HTMLElement
 
 type clientProfile = {
   id: string
@@ -23,27 +25,27 @@ type clientProfile = {
   email: string
   birth: string
   personalTrainerID: string
+  personalTrainerName: string
   trainingPlanID: string
+  trainingPlanName: string
   discountID: string
+  discountName: string
 }
 
 export function ClientProfile() {
   const getClientUrl = 'http://localhost:5000/api/client/'
-  // const {}
 
   const { id } = useParams()
   const navigate = useNavigate()
   // const
 
+  const [paymentIsShow, setPaymentIsShow] = useState(false)
 
-  const [paymentIsShow,setPaymentIsShow] = useState(false);
-
-
-  const showPaymentHandler = () =>{
-    setPaymentIsShow(true);
+  const showPaymentHandler = () => {
+    setPaymentIsShow(true)
   }
 
-  const closePaymentHandler = () =>{
+  const closePaymentHandler = () => {
     setPaymentIsShow(false)
   }
 
@@ -51,85 +53,95 @@ export function ClientProfile() {
     useState<clientProfile>()
 
   useEffect(() => {
-    axios
-      .get(getClientUrl + id)
-      .then((response: AxiosResponse) => {
-        setClientInfo(response.data)
-      })
+    async function loadClient() {
+      const clientData = await axios.get(getClientUrl + id)
+
+      setClientInfo(clientData.data)
+    }
+
+    loadClient()
   }, [id, navigate])
 
   return (
     <>
-    {
-      paymentIsShow && ReactDOM.createPortal(
-        <>
-        <Backdrop onClose={closePaymentHandler}/>
-        <Payment onClose={closePaymentHandler}/>
-        </>,portalElement
-      )
-    }
-    <DefaultPage>
-      <DefaultInsidePage>
-        <div className={styles.headerContainer}>
-          <div className={styles.left}>
-            <button
-              onClick={() => {
-                navigate(-1)
-              }}
-            >
-              <img src={whiteBack} alt="Back" />
-            </button>
+      {paymentIsShow &&
+        ReactDOM.createPortal(
+          <>
+            <Backdrop onClose={closePaymentHandler} />
+            <Payment onClose={closePaymentHandler} />
+          </>,
+          portalElement,
+        )}
+      <DefaultPage>
+        <DefaultInsidePage>
+          <div className={styles.headerContainer}>
+            <div className={styles.left}>
+              <button
+                onClick={() => {
+                  navigate(-1)
+                }}
+              >
+                <img src={whiteBack} alt="Back" />
+              </button>
+            </div>
+            <h1>Informação Do Cliente</h1>
           </div>
-          <h1>Informação Do Cliente</h1>
-        </div>
-        <div className={styles.clientInfoContainer}>
-          <div className={styles.infoContainer}>
-            <label>Nome:</label>
-            <p>{clientInfo?.name}</p>
-          </div>
-          <div className={styles.infoContainer}>
-            <label>Email:</label>
-            <p>{clientInfo?.email}</p>
-          </div>
-          <div className={styles.infoContainer}>
-            <label>Data de Nascimento:</label>
-            <p>{clientInfo?.birth}</p>
-          </div>
+          <div className={styles.clientInfoContainer}>
+            <div className={styles.infoContainer}>
+              <label>Nome:</label>
+              <p>{clientInfo?.name}</p>
+            </div>
+            <div className={styles.infoContainer}>
+              <label>Email:</label>
+              <p>{clientInfo?.email}</p>
+            </div>
+            <div className={styles.infoContainer}>
+              <label>Data de Nascimento:</label>
+              <p>{clientInfo?.birth}</p>
+            </div>
 
-          <div className={styles.infoContainer}>
-            <div className={styles.chooseContainer}>
-              <label>Plano de treino:</label>
-              <p>{clientInfo?.trainingPlanID}</p>
+            <div className={styles.infoContainer}>
+              <div className={styles.chooseContainer}>
+                <label>Plano de treino:</label>
+                <p>
+                  {clientInfo?.trainingPlanName}
+                  {/* {trainingPlanName} */}
+                </p>
+              </div>
+              <div className={styles.btnContainer}>
+                {/* <button>Alterar</button> */}
+              </div>
             </div>
-            <div className={styles.btnContainer}>
-              <button>Alterar</button>
+            <div className={styles.infoContainer}>
+              <div className={styles.chooseContainer}>
+                <label>Personal Trainer:</label>
+                <p>{clientInfo?.personalTrainerName}</p>
+              </div>
+              <div className={styles.btnContainer}>
+                {/* <button>Alterar</button> */}
+              </div>
             </div>
+            <div className={styles.infoContainer}>
+              <div className={styles.chooseContainer}>
+                <label>Desconto:</label>
+                <p>{clientInfo?.discountName}</p>
+              </div>
+              <div className={styles.btnContainer}>
+                {/* <button>Alterar</button> */}
+              </div>
+            </div>
+            {/* <button>Pagamento</button> */}
           </div>
-          <div className={styles.infoContainer}>
-            <div className={styles.chooseContainer}>
-              <label>Personal Trainer:</label>
-              <p>{clientInfo?.personalTrainerID}</p>
-            </div>
-            <div className={styles.btnContainer}>
-              <button>Alterar</button>
-            </div>
-          </div>
-          <div className={styles.infoContainer}>
-            <div className={styles.chooseContainer}>
-              <label>Plano de treino:</label>
-              <p>{clientInfo?.discountID}</p>
-            </div>
-            <div className={styles.btnContainer}>
-              <button>Alterar</button>
-            </div>
-          </div>
-          {/* <button>Pagamento</button> */}
+        </DefaultInsidePage>
+        <div className={styles.btnPayContainer}>
+          <button
+            className={styles.btnPay}
+            onClick={showPaymentHandler}
+          >
+            Pagamento
+          </button>
         </div>
-      </DefaultInsidePage>
-      <div className={styles.btnPayContainer}>
-        <button className={styles.btnPay} onClick={showPaymentHandler}>Pagamento</button>
-      </div>
-    </DefaultPage>
+      </DefaultPage>
     </>
   )
 }
--- client/src/Components/Clients/page/Payment/index.tsx
+++ client/src/Components/Clients/page/Payment/index.tsx
@@ -1,17 +1,86 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import styles from './styles.module.scss'
 
 import whiteCross from '../../../../assets/icons/whiteCross.svg'
 import blueCross from '../../../../assets/icons/blueCross.svg'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
 
 type propsType = {
   onClose: () => void
 }
 
+type PaymentInfo = {
+  personalTrainerId: string,
+  personalTrainerName: string
+  persontalTrainerValue: string
+  trainingPlanId: string
+  trainingPlanName: string
+  trainingPlanValue: string
+  discountPlanId: string
+  discountName: string
+  discountValue: string
+}
+
 export const Payment = (props: propsType) => {
+  const getPaymentUrl = 'http://localhost:5000/api/payment/'
+
+  const postPaymentUrl = "http://localhost:5000/api/payment/add"
+
+  const { id } = useParams()
+
+  // console.log(id);
+
+  const [paymentInfo, setPaymentInfo] =
+    useState<PaymentInfo>()
+
+  const [total, setTotal] = useState<string>()
+
+  useEffect(() => {
+    // const getInfo = axios(getClientUrl + id)
+
+    async function loadPayment() {
+      const getInfo = await axios.get(getPaymentUrl + id)
+
+      setPaymentInfo(getInfo.data)
+    }
+
+    loadPayment()
+  }, [])
+
+  useEffect(() => {
+
+     const totalValue = Number(paymentInfo?.trainingPlanValue) + Number(paymentInfo?.persontalTrainerValue) - Number(paymentInfo?.discountValue)
+    setTotal(totalValue.toString())
+  }, [paymentInfo])
+
+
+  // console.log(paymentInfo)
+
+  const formAddPaymentHandler = async (event: React.FormEvent<HTMLFormElement>) =>{
+
+    event.preventDefault()
+    const formData = {
+      clientId: id,
+      trainingPlanId : paymentInfo?.trainingPlanId,
+      personalTrainerId: paymentInfo?.personalTrainerId,
+      discountId: paymentInfo?.discountPlanId
+    }
+
+    // console.log(formData)
+    try {
+      await axios.post(postPaymentUrl,formData)
+      props.onClose()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const [closeButtonIsHover, setCloseButtonIsHover] =
     useState(false)
+
 
   const hoverHandler = () => {
     setCloseButtonIsHover(true)
@@ -42,39 +111,43 @@ export const Payment = (props: propsType) => {
           </button>
         </div>
       </div>
-      <div className={styles.formContainer}>
+      <form onSubmit={formAddPaymentHandler} 
+      className={styles.formContainer}>
         <div className={styles.title}>
           <h2>Detalhes</h2>
           <h2>Preço</h2>
         </div>
         <div className={styles.itemContainer}>
           <p>
-            <strong>Plano de Treino:</strong>treino 1
+            <strong>Plano de Treino:</strong>
+            {paymentInfo?.trainingPlanName}
           </p>
-          <p>110$00</p>
+          <p>{paymentInfo?.trainingPlanValue}$00</p>
         </div>
         <div className={styles.itemContainer}>
           <p>
-            <strong>Personal Trainer:</strong>personal 1
+            <strong>Personal Trainer:</strong>
+            {paymentInfo?.personalTrainerName}
           </p>
-          <p>110$00</p>
+          <p>{paymentInfo?.persontalTrainerValue}$00</p>
         </div>
         <div className={styles.itemContainer}>
           <p>
-            <strong>Desconto:</strong> desconto 1
+            <strong>Desconto:</strong>
+            {paymentInfo?.discountName}
           </p>
-          <p>10$00</p>
+          <p>{paymentInfo?.discountValue}$00</p>
         </div>
         <div className={styles.itemContainer}>
           <p>
             <strong>Total:</strong>
           </p>
-          <p>210$00</p>
+          <p>{total}$00</p>
         </div>
         <div className={styles.btnSubmitContainer}>
           <button>Confirmar</button>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
